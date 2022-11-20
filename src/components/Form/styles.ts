b1459ff93ef5ddaa5b76f9193b0f8b672fc53dfb
rafaelvm <vieiramrafael@gmail.com>
2022-11-20 20:31:00 -0300

    fix: adjusted styles and project title

--- src/components/Form/styles.ts
+++ src/components/Form/styles.ts
@@ -4,7 +4,7 @@ export const FormContainer = styled.form`
   flex-grow: 2;
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.3);
   z-index: 1;
-  min-height: 100vh;
+  min-height: 95vh;
   display: flex;
   flex-direction: column;
   padding: 12px 40px;
