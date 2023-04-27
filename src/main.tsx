50818097c9a371642bd9518d8ae020e66d2687e7
rafaelvm <vieiramrafael@gmail.com>
2023-04-26 22:58:29 -0300

    fix: adjusted providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { AuthProvider } from "../src/context/Auth";
+import theme from "./styles/theme";
 import App from "./App";
+import { Router } from "../src/router";
+import { NavBar } from "../src/components/NavBar";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme={theme}>
+        <AuthProvider>
+          <div className="container">
+            <App />
+            <NavBar />
+            <Router />
+          </div>
+        </AuthProvider>
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
